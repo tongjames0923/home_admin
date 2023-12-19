fa6d3506e379c8b6b18dd731966b1abd1f2ac595
tongjames0923 <tongjames@live.com>
2023-12-19 16:52:46 +0800

    管理员登录基本雏形

---
+++ src/store/modules/user.js
@@ -0,0 +1,110 @@
+import { login, logout, getInfo, as_admin } from '@/api/user'
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { resetRouter } from '@/router'
+
+const getDefaultState = () => {
+  return {
+    token: getToken(),
+    name: '',
+    avatar: '',
+    is_admin: false
+  }
+}
+
+const state = getDefaultState()
+
+const mutations = {
+  RESET_STATE: (state) => {
+    Object.assign(state, getDefaultState())
+  },
+  SET_TOKEN: (state, token) => {
+    state.token = token
+  },
+  SET_NAME: (state, name) => {
+    state.name = name
+  },
+  SET_AVATAR: (state, avatar) => {
+    state.avatar = avatar
+  },
+
+  SET_CHARATER: (state, is_admin) => {
+    state.is_admin = is_admin
+  }
+}
+
+const actions = {
+  // user login
+  login({ commit }, userInfo) {
+    const { username, password } = userInfo
+    return new Promise((resolve, reject) => {
+      login({ username: username.trim(), password: password })
+        .then((response) => {
+          // const { data } = response
+          // commit('SET_TOKEN', data)
+          // setToken(data.token)
+          resolve()
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  },
+
+  // user login
+  adminLogin({ commit }, token) {
+    return new Promise((resolve, reject) => {
+      as_admin(token)
+        .then((response) => {
+          const { data } = response
+          commit('SET_TOKEN', data)
+          commit('SET_CHARATER', true)
+          setToken(data)
+          resolve()
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  },
+
+  // get user info
+  getInfo({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      commit('SET_NAME', '用户你好')
+      // commit('SET_AVATAR', avatar)
+      resolve('用户你好')
+    })
+  },
+
+  // user logout
+  logout({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      logout(state.token)
+        .then(() => {
+          removeToken() // must remove  token  first
+          resetRouter()
+          commit('RESET_STATE')
+          resolve()
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  },
+
+  // remove token
+  resetToken({ commit }) {
+    return new Promise((resolve) => {
+      removeToken() // must remove  token  first
+      commit('RESET_STATE')
+      resolve()
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
